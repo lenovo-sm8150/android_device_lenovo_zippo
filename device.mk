a904e93cb9f94602824e22713c5c1e297e27f0d9
Marc Bourgoin <themard85@gmail.com>
2020-12-08 04:32:08 -0700

    zippo: Give our lights HAL a more unique name and address selinux
    Change-Id: Ie8d904b760236b2d92974bd1b5701d1b526d36f9

--- device.mk
+++ device.mk
@@ -268,7 +268,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.lenovo
+    android.hardware.light@2.0-service.lenovo_zippo
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
