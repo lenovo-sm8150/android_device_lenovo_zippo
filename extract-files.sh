ce33aa7c93931fd7c813e089d29c47ffd5f13b4b
Lucchetto <lucchetto.tie@live.com>
2021-01-28 17:24:07 +0100

    sm8150-common: sync extract scripts with templates
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- extract-files.sh
+++ extract-files.sh
@@ -6,13 +6,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
-# Required!
 export DEVICE=zippo
 export DEVICE_COMMON=sm8150-common
 export VENDOR=lenovo
 
-export DEVICE_BRINGUP_YEAR=2019
-
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,11 +8,8 @@
 
 set -e
 
-# Required!
 export DEVICE=zippo
 export DEVICE_COMMON=sm8150-common
 export VENDOR=lenovo
-
-export DEVICE_BRINGUP_YEAR=2019
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
