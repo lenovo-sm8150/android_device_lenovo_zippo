4fe2ee10e6702c512f2c8a52b61f89c2123d3fa8
Lucchetto <lucchetto.tie@live.com>
2021-02-21 19:35:43 +0100

    zippo: lineage
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_zippo.mk
+    $(LOCAL_DIR)/lineage_zippo.mk
--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -6,8 +6,8 @@
 
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
-# Inherit some common RevengeOS stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
@@ -15,7 +15,7 @@ PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z6 Pro
-PRODUCT_NAME := revengeos_zippo
+PRODUCT_NAME := lineage_zippo
 
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
