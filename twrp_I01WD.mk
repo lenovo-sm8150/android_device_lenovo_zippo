41b5583717ed031b2685903cd11790860d76084d
micky387 <mickaelsaibi@free.fr>
2021-09-30 10:54:02 +0200

    I01WD: include inherit core_64_bit.mk
    Change-Id: I799e2fafe12c14ce99c4faca07b5794457535859

--- twrp_I01WD.mk
+++ twrp_I01WD.mk
@@ -29,6 +29,7 @@ CUSTOM_VENDOR := $(lastword $(subst /, ,$(firstword $(subst _, ,$(firstword $(MA
 
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
