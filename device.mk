8e5363a5092cfebb0bed8d376c4c7d6aef632cd0
micky387 <mickaelsaibi@free.fr>
2021-10-02 14:35:56 +0200

    I01WD:  Add android.hardware.health@2.1 files
    Change-Id: Ie7531d388ecb798109677cdd2ae17056aeac4c73

--- device.mk
+++ device.mk
@@ -28,6 +28,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl.recovery \
+    android.hardware.health@2.1-impl.recovery \
     bootctrl.$(PRODUCT_PLATFORM).recovery
 
 PRODUCT_COPY_FILES += \
