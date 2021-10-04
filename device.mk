bec9ab0583edb1ab5e7b1c67fcbca3866e05154b
micky387 <mickaelsaibi@free.fr>
2021-10-04 14:39:41 +0200

    I01WD: Downgrade hardware boot to 1.0
    It cause failure to switch to other inactive slot after flashing
    
    Change-Id: I50a9807211cc593bd03a4b023802907e75b28ef8

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.0-impl.recovery \
     android.hardware.health@2.1-impl.recovery \
     bootctrl.$(PRODUCT_PLATFORM).recovery
 
