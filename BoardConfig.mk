4c2f849c2b6db291f76748389c8df466503202b1
Captain Throwback <captainthrowback@hotmail.com>
2021-11-11 11:19:09 -0500

    Updates for A12 support
    Change-Id: Ifea618e1aa6ec93fdb1610490d2d9bd3299c8aaa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Encryption
 PLATFORM_VERSION := 80
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 TW_INCLUDE_CRYPTO := true
 BOARD_USES_METADATA_PARTITION := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
@@ -184,7 +185,7 @@ TW_NO_SCREEN_BLANK := true
 TW_HAS_EDL_MODE := true
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+    "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 LZMA_RAMDISK_TARGETS := recovery
 BOARD_RAMDISK_USE_LZMA := true
 LZMA_COMPRESSION := -9
