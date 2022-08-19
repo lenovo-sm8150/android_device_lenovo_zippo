baddb4a97f51e91e6f2fbe470f3de4b2ce01d456
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:39:47 +0200

    zippo: use libhidlbase from vndk v32 for libvendor.goodix.hardware.biometrics.fingerprint@2.1.so
        * _ZN7android8hardware7details17gBnConstructorMapE and _ZN7android8hardware7details17gBsConstructorMapE have been removed on Android 13
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        # Use VNDK 32 libhidlbase
+        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
